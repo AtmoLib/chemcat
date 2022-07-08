9cd59290bed3e79515003b892dfd298ddad4ab6c
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2022-07-08 20:34:18 +0200

    Added some basic files.

---
+++ chemcat/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 Cubillos and Blecic
+# chemcat is open-source software under the GPL-2.0 license (see LICENSE)
+
+__all__ = [
+]
+
+from .version import __version__
+
+
+# Clean up top-level namespace--delete everything that isn't in __all__
+# or is a magic attribute, and that isn't a submodule of this package
+for varname in dir():
+    if not ((varname.startswith('__') and varname.endswith('__')) or
+            varname in __all__):
+        del locals()[varname]
+del(varname)
---
+++ chemcat/version.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022 Cubillos and Blecic
+# chemcat is open-source software under the GPL-2.0 license (see LICENSE)
+
+# chemcat version:
+__version__ = '0.0.1'
+
