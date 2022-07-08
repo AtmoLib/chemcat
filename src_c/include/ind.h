80f7359d8726f9440490aaeda236de60304dcf15
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2022-07-08 22:02:40 +0200

    Added C hearder file ind.h, which will be used later.

---
+++ src_c/include/ind.h
@@ -0,0 +1,24 @@
+// This file is open-source software under the
+// GNU GENERAL PUBLIC LICENSE Version 2, June 1991
+
+// Definitions for indexing Numpy arrays:
+
+// 1D double ndarray:
+#define INDd(a,i) *((double *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+
+// 2D double ndarray:
+#define IND2d(a,i,j) *((double *)( \
+    PyArray_DATA(a)  + i * PyArray_STRIDE(a, 0) + j * PyArray_STRIDE(a, 1)))
+
+// 3D double ndarray:
+#define IND3d(a,i,j,k) *((double *)( \
+    PyArray_DATA(a) \
+    + i * PyArray_STRIDE(a, 0) \
+    + j*PyArray_STRIDE(a, 1) \
+    + k*PyArray_STRIDE(a, 2)))
+
+// 1D integer ndarray:
+#define INDi(a,i) *((int *)(PyArray_DATA(a) + i * PyArray_STRIDE(a, 0)))
+// 2D integer ndarray:
+#define IND2i(a,i,j) *((int *)( \
+    PyArray_DATA(a) + i * PyArray_STRIDE(a, 0) + j * PyArray_STRIDE(a, 1)))
