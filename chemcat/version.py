b134e9212ac9a4267178c74fb1b74cb8b47d48a6
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2022-07-15 14:53:09 +0200

    Updated to chemcat version 0.1.0.

--- chemcat/version.py
+++ chemcat/version.py
@@ -1,6 +1,6 @@
-# Copyright (c) 2022 Cubillos and Blecic
+# Copyright (c) 2022 Blecic and Cubillos
 # chemcat is open-source software under the GPL-2.0 license (see LICENSE)
 
 # chemcat version:
-__version__ = '0.0.2'
+__version__ = '0.1.0'
 
