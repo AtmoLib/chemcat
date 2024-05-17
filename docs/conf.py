cd8876dd9047811baf7389661cc6e3856d2be188
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-05-17 17:43:36 +0200

    Introducing jupyter notebook docs.

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+#    'sphinx.ext.autosectionlabel',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
